2a5053c98c13fbf9819e8d1fec0076a91fad898c
parora54 <pranava880@gmail.com>
2024-08-17 00:18:54 -0400

    Add: Basic Routing Logic, Navbar + Login .jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import Home from "./Home.jsx";
+import Login from "./Login.jsx";
+import Navbar from "./components/Navbar.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Navbar />
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
