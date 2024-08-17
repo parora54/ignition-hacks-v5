b93d670c45c260f1d9086b3a81fa5d19ab9beefc
Blazzen <devan@s0dhi.com>
2024-08-17 10:17:00 -0400

    Added Login API
    added basic login feature in app.py

---
+++ server/seed.py
@@ -0,0 +1,13 @@
+from app import app, db
+from models import User
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
+
+with app.app_context():
+    db.create_all()
+    hashed_password = bcrypt.generate_password_hash('password123').decode('utf-8')
+    user = User(email='test@example.com', password=hashed_password)
+    db.session.add(user)
+    db.session.commit()
+    print("done")
