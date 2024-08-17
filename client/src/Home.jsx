76ac3034619760cc5a2048884183ec089563c800
parora54 <pranava880@gmail.com>
2024-08-17 19:35:31 -0400

    add: basic home page

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,8 +1,45 @@
 function Home() {
   return (
-    <>
-      <div>This is the Home Page</div>
-    </>
+    <div className="home-container">
+      <h1 className="welcome-text">Welcome to ?BizTechHub?</h1>
+      <p className="subheading">
+        Your Gateway to a Thriving Career in Business and Technology
+      </p>
+
+      <section className="feature-section">
+        <div className="feature">
+          <h2>Explore Career Opportunities</h2>
+          <p>
+            Dive into the world of business and tech, and discover the paths
+            that align with your passions and goals.
+          </p>
+        </div>
+
+        <div className="feature">
+          <h2>Prepare for Success</h2>
+          <p>
+            Equip yourself with top-notch resources and strategies for
+            hackathons and case competitions.
+          </p>
+        </div>
+
+        <div className="feature">
+          <h2>Tailored Competitions</h2>
+          <p>
+            Find and conquer challenges that match your education and experience
+            level, designed to push you forward.
+          </p>
+        </div>
+
+        <div className="feature">
+          <h2>Track Your Achievements</h2>
+          <p>
+            Monitor your progress, earn accolades, and propel yourself towards
+            excellence in your field.
+          </p>
+        </div>
+      </section>
+    </div>
   );
 }
 
