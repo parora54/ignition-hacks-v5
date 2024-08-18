cbbc8ce4cbb9c8f9c1a1c16280a0cd3cb2809f4b
parora54 <pranava880@gmail.com>
2024-08-18 03:14:12 -0400

    test commit

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -3,7 +3,7 @@ import styles from "./styles/Home.module.css";
 function Home() {
   return (
     <div className={styles.homeContainer}>
-      <h1 className={styles.welcomeText}>Welcome to Aspire</h1>
+      <h1 className={styles.welcomeText}>Welcome to as</h1>
       <p className={styles.subheading}>
         Your Gateway to a Thriving Career in Business and Technology
       </p>
